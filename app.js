44f766e6aebc9078df85b4f979fd16229baad47b
Nathan Heller <gritty.desk3330@fastmail.com>
2025-08-13 18:33:46 -0400

    extract readable text and create basic feed xml

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
-const { Readability } = require ('@mozilla/readability')
+const { Readability, isProbablyReaderable } = require ('@mozilla/readability')
 const createDOMPurify = require('dompurify');
 const jsdom = require("jsdom");
+const RSS = require('rss');
+const fs = require("fs");
 
 const { JSDOM } = jsdom;
 
-const url = 'https://hackaday.com/2025/08/04/happy-birthday-6502/'
-
-// const article = new Readability({}).parse();
-
-const getArticle = async () => {
-    const dom = await JSDOM.fromURL(url, {})
-    const DOMPurify = createDOMPurify(dom.window);
-    const document = dom.window.document
-    const article = new Readability(document).parse();
-    console.log('article', article)
-    const cleanArticle = DOMPurify.sanitize(article, { USE_PROFILES: { html: true } });
-    // console.log('cleanArticle', cleanArticle)
-    return cleanArticle
+const getFeed = () => {
+    return new RSS({
+        title: 'My feed',
+        link: 'https://example.com',
+        description: 'A feed for my saved articles',
+    })
 }
 
-const articleHtml = getArticle()
+const getFeedItemFromUrl = async (url) => {
+    const dom = await JSDOM.fromURL(url, {})
+    const DOMPurify = createDOMPurify(dom.window);
+    const doc = dom.window.document;
+
+    // TODO: Use isReadable here?
+    const article = new Readability(doc).parse();
+    const cleanArticleContent = DOMPurify.sanitize(article.content, { USE_PROFILES: { html: true } });
+
+    return {
+        title: article.title,
+        description: cleanArticleContent,
+        author: article.byline,
+        url,
+        date: article.publishedTime,
+    }
+}
+
+const url1 = 'https://hackaday.com/2025/07/29/power-line-patrols-the-grids-eye-in-the-sky/'
+const url2 = 'https://www.wired.com/story/war-of-the-worlds-isnt-just-bad-its-also-shameless-tech-propaganda/'
+const url3 = 'https://techcrunch.com/2025/08/13/pebbles-smartwatch-is-back-pebble-time-2-specs-revealed/'
+
+const main = async () => {
+    const myFeed = getFeed()
+    const feedItem1 = await getFeedItemFromUrl(url1)
+    const feedItem2 = await getFeedItemFromUrl(url2)
+    const feedItem3 = await getFeedItemFromUrl(url3)
+
+    // TODO: Decide how to order feed items
+    myFeed.item(feedItem1)
+    myFeed.item(feedItem2)
+    myFeed.item(feedItem3)
+
+    const xml = myFeed.xml();
+
+    fs.writeFile("./feed.xml", xml, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // File written successfully
+        }
+    });
+}
+
+main()
--- app2.js
+++
@@ -1,3 +0,0 @@
-const { article } = require('./article.js')
-console.log(article)
-
